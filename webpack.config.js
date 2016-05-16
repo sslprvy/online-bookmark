2005b1f605089a12bb5d571d21270d9f1f35fd09
Csaba Valyi <csaba.valyi1@gmail.com>
2016-05-16 22:04:39 +0200

    webpack setup complete

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname+'/build',
+        filename: 'online-bookmark.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
