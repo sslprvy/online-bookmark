2333e89ea182fac278bce43b7950f7fc1192af2c
Csaba Valyi <csaba_valyi@epam.com>
2016-05-23 13:18:06 +0200

    mock data with url created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-import { OnlineBookmark } from './online-bookmark.jsx';
+// FIXME mocked data: remove it when no longer needed
+console.log(require('json!./bookmarks.json'));
+
+// SASS entry point
+require('./online-bookmark.scss');
+
+
+require('./online-bookmark.jsx');
