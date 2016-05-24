ae40e0853108c10fe7912efaafd8788c5201a106
Csaba Valyi <csaba_valyi@epam.com>
2016-05-24 13:32:13 +0200

    removed class

--- src/bookmark-element.jsx
+++ src/bookmark-element.jsx
@@ -7,7 +7,6 @@ export class BookmarkElement extends React.Component {
 
         return (
             <li className="bookmark-element">
-                <span className="ion-arrow-right-c"></span>
                 <div className="bookmark-element-title">{ title }</div>
                 <a className="bookmark-element-url" href={ url } target="_blank">{ url }</a>
                 <div className="bookmark-element-tags">{ tags.toString() }</div>
