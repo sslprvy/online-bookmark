9042b4230313f82152d053129a05e384fe3dd6f0
Peter Seprus <peter_seprus@epam.com>
2016-11-15 16:55:06 +0100

    data_structure: Add basic list rendering

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-export default class Hello extends React.Component {
+// Please, see the data-structure-proposal.json file
+// for further information
+var mockData = {
+    isUeser: true,
+    isMutable: true,
+    data: [
+        {
+            title: "Example",
+            url: "www.example.com",
+            tags: [
+                "quickFilterTag",
+                "anotherQuickFilterTag"
+            ]
+        },
+        {
+            title: "AnotherExample",
+            url: "www.anotherexample.com",
+            tags: [
+                "anotherQuickFilterTag"
+            ]
+        }
+    ]
+};
+
+class List extends React.Component {
+    render() {
+        var urlObjects = this.props.objects.map(function (urlObject) {
+            var tagObjects = urlObject.tags.map(function (tag) {
+                return (<span className="display-list-element-tag">{tag}</span>)
+            });
+            return (
+                <li className="display-list-element">
+                    <div>
+                        <span className="display-list-element-title">{urlObject.title}</span><br/>
+                        <span className="display-list-element-url">{urlObject.url}</span><br/>
+                        <span>{tagObjects}</span>
+                    </div>
+                </li>
+            )
+        });
+        return <ul className="display-list">{urlObjects}</ul>
+    }
+};
+
+export default class Main extends React.Component {
     constructor() {
         super();
     }
-
     render() {
-        return(
-            <div>Hello World</div>
-        )
+        return (<div><List objects={this.props.userData.data} /></div>)
     }
 }
 
-ReactDom.render(<Hello />, document.getElementById('app-content'));
+ReactDom.render(<Main userData={mockData} />, document.getElementById('app-content'));
