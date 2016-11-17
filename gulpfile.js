6c254731bf35cfe88e282eee9338502ef6dad81e
Peter Seprus <peter_seprus@epam.com>
2016-11-17 15:27:36 +0100

    Remove unnecessary comment

--- gulpfile.js
+++ gulpfile.js
@@ -36,10 +36,6 @@ const CONFIG = {
 // ----------------------------------------------------------------------------
 
 gulp.task('lint', function (callback) {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
     return gulp.src(['app/**/*.jsx'])
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
