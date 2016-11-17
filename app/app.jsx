9dddf82ddac981eb29feaaa2c6e4f298da65f324
Csaba Valyi <csaba_valyi@epam.com>
2016-11-17 15:01:56 +0100

    Extract List to its own file

--- app/app.jsx
+++ app/app.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import mockJson from '../data-structure-proposal.json';
+import { List } from './list.jsx';
 
-class List extends React.Component {
-    render() {
-        var urlObjects = this.props.objects.map(function (urlObject) {
-            var tagObjects = urlObject.tags.map(function (tag, index) {
-                var key = `${urlObject.title}-tag-${index}`;
-                return (<span className="display-list-element-tag" key={key}>{tag}</span>)
-            });
-            return (
-                <li className="display-list-element" key={urlObject.title}>
-                    <div>
-                        <span className="display-list-element-title">{urlObject.title}</span><br/>
-                        <span className="display-list-element-url">{urlObject.url}</span><br/>
-                        <span>{tagObjects}</span>
-                    </div>
-                </li>
-            )
-        });
-        return <ul className="display-list">{urlObjects}</ul>
-    }
-};
+import mockJson from '../data-structure-proposal.json';
 
 export default class Main extends React.Component {
     constructor() {
---
+++ app/list.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export class List extends React.Component {
+    render() {
+        var urlObjects = this.props.objects.map(function (urlObject, index) {
+            var listElementKey = `${urlObject}-${index}`;
+            return (<ListElement data={urlObject} key={listElementKey}/>)
+        });
+
+        return (<ul className="display-list">{urlObjects}</ul>)
+    }
+};
+
+class ListElement extends React.Component {
+    render() {
+        var tagObjects = this.props.data.tags.map((tag, index) => {
+            var key = `${this.props.data.title}-tag-${index}`;
+            return (<span className="display-list-element-tag" key={key}>{tag}</span>)
+        });
+        return (
+                <li className="display-list-element" key={this.props.data.title}>
+                    <div>
+                        <span className="display-list-element-title">{this.props.data.title}</span><br/>
+                        <span className="display-list-element-url">{this.props.data.url}</span><br/>
+                        <span>{tagObjects}</span>
+                    </div>
+                </li>
+        )
+    }
+}
