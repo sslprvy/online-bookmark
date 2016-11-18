e4c5624b30f6ac73c1eb1007c21fe088be6efa60
Peter Seprus <peter_seprus@epam.com>
2016-11-18 14:40:15 +0100

    Add missing spaces

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,7 @@ function bundleApp(isProduction) {
     // If it's not for production, a separate vendors.js file will be created
     // the first time gulp is run so that we don't have to rebundle things like
     // react everytime there's a change in the js file
-    if (!isProduction && scriptsCount === 1){
+    if (!isProduction && scriptsCount === 1) {
         // create vendors.js for dev environment.
         browserify({
             require: dependencies,
@@ -149,7 +149,7 @@ function bundleApp(isProduction) {
             .pipe(source('vendors.js'))
             .pipe(gulp.dest(CONFIG.tempFolder));
     }
-    if (!isProduction){
+    if (!isProduction) {
         // make the dependencies external so they dont get bundled by the
         // app bundler. Dependencies are already bundled in vendor.js for
         // development environments.
