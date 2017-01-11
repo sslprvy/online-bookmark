f90a0154540f74c35544ec9c68a13dba79da2fd2
Csaba Valyi <csaba_valyi@epam.com>
2017-01-11 16:44:27 +0100

    Fetch data from json server

--- app/app.jsx
+++ app/app.jsx
@@ -3,17 +3,36 @@ import ReactDom from 'react-dom';
 
 import { List } from './components/list';
 
-import mockJson from './mock-data';
+import config from '../config';
 
 var USERNAME = 'kunstkammern';
 
 export default class App extends React.Component {
     constructor() {
         super();
+        this.state = {
+            userData: {
+                data: []
+            }
+        }
     }
+
+    componentDidMount() {
+        const request = new Request(`${config.path}/users`, {
+            method: 'GET'
+        });
+        fetch(request)
+            .then(response => response.json())
+            .then(users => {
+                this.setState({
+                    userData: users[USERNAME]
+                });
+            });
+    }
+
     render() {
-        return <div><List objects={this.props.userData.data} /></div>;
+        return <div><List objects={this.state.userData.data} /></div>;
     }
 }
 
-ReactDom.render(<App userData={mockJson[USERNAME]} />, document.getElementById('app-content'));
+ReactDom.render(<App />, document.getElementById('app-content'));
