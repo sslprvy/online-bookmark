2f79f4fc35593c021b0e1c658868d23a55340ffd
Csaba Valyi <csaba_valyi@epam.com>
2017-01-17 16:56:30 +0100

    Define edit entry reducer

---
+++ app/reducers/edit-entry.js
@@ -0,0 +1,22 @@
+const defaultState = {
+    editing: false,
+    underEdit: ''
+};
+
+export default function editEntry(state = defaultState, action) {
+    switch (action.type) {
+        case 'EDIT_ENTRY':
+            return {
+                editing: true,
+                underEdit: action.id
+            };
+        case 'SAVE_ENTRY': {
+            return {
+                editing: false,
+                underEdit: ''
+            };
+        }
+        default:
+            return state;
+    }
+}
