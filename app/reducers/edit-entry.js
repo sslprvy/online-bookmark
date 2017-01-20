e1d53c48f7d5fa4eb8662dd288e26f857eab72bb
Peter Seprus <peter_seprus@epam.com>
2017-01-20 18:11:45 +0100

    OB-6: Update spaces

--- app/reducers/edit-entry.js
+++ app/reducers/edit-entry.js
@@ -5,19 +5,19 @@ const defaultState = {
 
 export default function editEntry(state = defaultState, action) {
     switch (action.type) {
-        case 'UPDATE_USER':
-        case 'EDIT_ENTRY':
-            return {
-                editing: true,
-                underEdit: action.id
-            };
-        case 'SAVE_ENTRY': {
-            return {
-                editing: false,
-                underEdit: ''
-            };
-        }
-        default:
-            return state;
+    case 'UPDATE_USER':
+    case 'EDIT_ENTRY':
+        return {
+            editing: true,
+            underEdit: action.id
+        };
+    case 'SAVE_ENTRY': {
+        return {
+            editing: false,
+            underEdit: ''
+        };
+    }
+    default:
+        return state;
     }
 }
