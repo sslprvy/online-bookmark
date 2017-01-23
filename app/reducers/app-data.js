ce7ddb6cd8e046046a663033550e24d91d21df15
Peter Seprus <peter_seprus@epam.com>
2017-01-23 10:01:21 +0100

    OB-6: Fix indentation issue with switch case

--- app/reducers/app-data.js
+++ app/reducers/app-data.js
@@ -5,28 +5,28 @@ const defaultState = {
 
 export default function appData(state = defaultState, action) {
     switch (action.type) {
-    case 'FETCHING_DATA':
-        return {
-            isFetching: true,
-            users: state.users
-        };
-    case 'RECEIVED_DATA':
-        return {
-            isFetching: false,
-            users: action.data
-        };
-    case 'UPDATE_USER':
-        return {
-            isFetching: false,
-            users: state.users.map(user => {
-                if (user.id === action.user.id) {
-                    return action.user;
-                } else {
-                    return user;
-                }
-            })
-        };
-    default:
-        return state;
+        case 'FETCHING_DATA':
+            return {
+                isFetching: true,
+                users: state.users
+            };
+        case 'RECEIVED_DATA':
+            return {
+                isFetching: false,
+                users: action.data
+            };
+        case 'UPDATE_USER':
+            return {
+                isFetching: false,
+                users: state.users.map(user => {
+                    if (user.id === action.user.id) {
+                        return action.user;
+                    } else {
+                        return user;
+                    }
+                })
+            };
+        default:
+            return state;
     }
 }
