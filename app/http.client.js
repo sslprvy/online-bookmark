cacb729a80f2d3ae8439fa8b0446512a6fff752e
Csaba Valyi <csaba_valyi@epam.com>
2017-02-21 08:39:39 +0100

    OB-17: Create createLink method

--- app/http.client.js
+++ app/http.client.js
@@ -21,3 +21,13 @@ export function saveUserData(user) {
 
     return fetch(request).then(response => response.json());
 }
+
+export function createLink(link, user) {
+    const highestId = user.data.reduce((accumulator, link) => {
+        return link.id > accumulator ? link.id : accumulator;
+    }, 0);
+    const modifiedLink = Object.assign({}, link, { id: highestId + 1 });
+    const modifiedUser = Object.assign({}, user, { data: user.data.concat(modifiedLink) });
+
+    return saveUserData(modifiedUser);
+}
