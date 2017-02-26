ecaf2fa941efee704eae371cd7a55206f412109d
Csaba Valyi <csaba.valyi1@gmail.com>
2017-02-26 18:12:23 +0100

    OB-24: Move db connection logic under api/helpers

---
+++ backend/online-bookmark/api/helpers/db-connection.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const argv = require('yargs').argv;
+
+const url = `mongodb://${argv.u}:${argv.pw}@${argv.ip}:${argv.p}/online-bookmark`;
+
+module.exports = {
+    connect
+}
+
+function connect() {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, handleConnect);
+        function handleConnect(err, db) {
+            if (err) {
+                console.error(err.MongoError);
+                reject('Couldn\'t connect to server');
+                throw Error('Couldn\'t connect to server');
+            }
+            console.log('Successfully connected to server');
+
+            resolve(db);
+
+            // // Find some documents in our collection
+            // db.collection('onlineBookmark').find({}).toArray(handleQuery.bind(null, db));
+
+            // // Declare success
+            // console.log('Called find()');
+        }
+
+
+    });
+}
