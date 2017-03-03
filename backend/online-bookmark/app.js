783a1f33d817bdd93e37eb6ee7cb5266cbd97366
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-03 18:17:21 +0100

    OB-24: Display on console the registered handlers

--- backend/online-bookmark/app.js
+++ backend/online-bookmark/app.js
@@ -1,23 +1,31 @@
 'use strict';
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+const SwaggerExpress = require('swagger-express-mw');
+const app = require('express')();
+const chalk = require('chalk');
+
 module.exports = app; // for testing
 
-var config = {
-  appRoot: __dirname // required config
+const config = {
+    appRoot: __dirname // required config
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+    const port = process.env.PORT || 10010;
+    app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    console.log('Registering handlers...');
+    Object.keys(swaggerExpress.runner.swagger.paths)
+        .filter(path => path !== '/swagger')
+        .forEach(path => {
+            console.log(chalk.magenta(path));
+            Object.keys(swaggerExpress.runner.swagger.paths[path])
+                .filter(method => method !== 'x-swagger-router-controller')
+                .forEach(method => console.log(`- ${chalk.cyan(method)}`));
+  });
 });
