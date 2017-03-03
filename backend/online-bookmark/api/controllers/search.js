2c7055c83a0b5bc5ea58e82ec4c05ad43b675800
Csaba Valyi <csaba_valyi@epam.com>
2017-03-03 14:35:12 +0100

    OB-24: Implement search title handler

--- backend/online-bookmark/api/controllers/search.js
+++ backend/online-bookmark/api/controllers/search.js
@@ -2,7 +2,8 @@ const DB = require('../helpers/db-connection');
 const handleQuery = require('../common/handle-simple-query');
 
 module.exports = {
-    searchTags
+    searchTags,
+    searchTitle
 };
 
 function searchTags(req, res) {
@@ -10,5 +11,13 @@ function searchTags(req, res) {
         const tags = req.swagger.params.tags.value.split(',');
 
         db.collection('links').find({ tags: { $in: tags } }).toArray(handleQuery.bind(null, db, res));
+    });
+}
+
+function searchTitle(req, res) {
+    DB.connect().then(db => {
+        const title = req.swagger.params.title.value;
+
+        db.collection('links').find({ title: { $regex: title , $options: 'i' }}).toArray(handleQuery.bind(null, db, res));
     });
 }
