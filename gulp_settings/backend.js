f80123f96eeca6e90f0635f57f7e069e50d50514
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-04 17:12:01 +0100

    OB-24: Create gulp task for starting up swagger backend

---
+++ gulp_settings/backend.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+const _ = require('lodash');
+
+let backendProcess;
+
+gulp.task('backend', (done) => {
+    if (!_.isUndefined(backendProcess)) {
+        backendProcess.on('exit', () => {
+            backendProcess = startProcess();
+            done();
+        })
+        backendProcess.kill('SIGINT');
+    } else {
+        backendProcess = startProcess();
+        done();
+    }
+});
+
+gulp.task('watch:backend', () => {
+    let backendProcess;
+
+    gulp.watch([
+        'backend/online-bookmark/api/**/*.js',
+        'backend/online-bookmark/api/**/*.yaml'
+        ], ['backend']);
+});
+
+function startProcess() {
+    return spawn('node', [
+            'backend/online-bookmark/app.js',
+            '--u freddy_krueger',
+            '--pw ThisIstheMostFuckedUpPasswordEver',
+            '--ip 185.111.88.74',
+            '--p 27017'
+        ],{ stdio: 'inherit' });
+}
