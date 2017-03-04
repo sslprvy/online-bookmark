eecefdd68115d9fd618cc230f1bdb359f78cbbe0
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-04 17:13:16 +0100

    OB-24: Implement createLink method

--- backend/online-bookmark/api/controllers/links.js
+++ backend/online-bookmark/api/controllers/links.js
@@ -2,11 +2,25 @@ const DB = require('../helpers/db-connection');
 const handleQuery = require('../common/handle-simple-query');
 
 module.exports = {
-    getLinks
+    getLinks,
+    createLink
 };
 
 function getLinks(req, res) {
     DB.connect().then(db => {
         db.collection('links').find({}).toArray(handleQuery.bind(null, db, res));
+    });
+}
+
+function createLink(req, res) {
+    DB.connect().then(db => {
+        const link = req.swagger.params.link.value;
+
+        db.collection('links')
+            .insert(link)
+            .then(({ ops: insertedLink }) => {
+                res.json(insertedLink);
+                db.close();
+            });
     });
 }
