9a3c969eb67651fdcce10484b7c4cdfc902d738f
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-04 19:17:22 +0100

    OB-24: Handle swagger error

--- gulp_settings/backend.js
+++ gulp_settings/backend.js
@@ -5,24 +5,36 @@ const _ = require('lodash');
 let backendProcess;
 
 gulp.task('backend', (done) => {
+    // This "hack" is needed because if swagger throws a yaml parse exception
+    // we wont start another process otherwise
+    // TODO: needs further enhancement since after an error every second "save" triggers a child process
+    let timeout = setTimeout(() => {
+        backendProcess = undefined;
+        done();
+    }, 100);
+
     if (!_.isUndefined(backendProcess)) {
         backendProcess.on('close', () => {
             backendProcess = startProcess();
+            clearTimeout(timeout);
             done();
         });
         backendProcess.kill('SIGTERM');
     } else {
         backendProcess = startProcess();
+        clearTimeout(timeout);
         done();
     }
 });
 
 gulp.task('watch:backend', () => {
-    let backendProcess;
-
-    gulp.watch(['backend/online-bookmark/api/**/*.yaml'], _.debounce(() => gulp.start(['backend']), 500));
+    gulp.watch('backend/online-bookmark/api/**/*.yaml').on('change', _.debounce(startBackendTask, 1000));
     gulp.watch('backend/online-bookmark/api/**/*.js', ['backend']);
 });
+
+function startBackendTask() {
+    return gulp.start(['backend']);
+}
 
 function startProcess() {
     return spawn('node', [
@@ -31,5 +43,5 @@ function startProcess() {
             '--pw ThisIstheMostFuckedUpPasswordEver',
             '--ip 185.111.88.74',
             '--p 27017'
-        ],{ stdio: 'inherit' });
+        ], { stdio: 'inherit' });
 }
