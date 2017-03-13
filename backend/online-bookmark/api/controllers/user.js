697a5f5e3437fc22e8ec77a104491c1cb9aaeda2
Csaba Valyi <csaba_valyi@epam.com>
2017-03-13 16:59:11 +0100

    OB-31: Implement handler for creating user

---
+++ backend/online-bookmark/api/controllers/user.js
@@ -0,0 +1,35 @@
+const DB = require('../helpers/db-connection');
+
+module.exports = {
+    createUser
+};
+
+function createUser(req, res) {
+    DB.connect().then(db => {
+        const user = req.swagger.params.user.value;
+        const { email, username } = user;
+
+        Promise.all([
+            db.collection('users').findOne({ email }),
+            db.collection('users').findOne({ username })
+        ]).then(([userByEmail, userByName]) => {
+            if (userByEmail || userByName) {
+                throw new Error(409);
+            }
+
+            return null;
+        })
+        .then(() => {
+            return db.collection('users').insert(user).then(() => true);
+        })
+        .then(() => {
+            res.json({ message: 'Success' });
+            db.close();
+        })
+        .catch(err => {
+            if (err.message === '409') {
+                res.status(409).send({ message: 'Username, or email address in use' });
+            }
+        });
+    });
+}
