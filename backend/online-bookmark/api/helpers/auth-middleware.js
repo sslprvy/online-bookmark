4d30a42d3a5fd2004cb0b8c8dd61a4812dd84089
Csaba Valyi <csaba_valyi@epam.com>
2017-03-15 15:35:29 +0100

    OB-31: Upon successful token validation update token for user and send back in header

--- backend/online-bookmark/api/helpers/auth-middleware.js
+++ backend/online-bookmark/api/helpers/auth-middleware.js
@@ -1,5 +1,6 @@
 const DB = require('./db-connection');
-const { verifyToken } = require('./crypto');
+const { verifyToken, generateToken } = require('./crypto');
+const { pick } = require('../helpers/common');
 
 const authExceptions = ['/user/login', 'verifyEmail/{token}', 'resendVerificationEmail/{token}'];
 
@@ -18,8 +19,20 @@ module.exports = function checkAuthorization(req, res, next) {
     const token = req.headers.authorization || req.headers.Authorization;
 
     verifyToken(token)
-        .then(() => {
-            next();
+        .then((decoded) => {
+            DB.connect().then(db => {
+                const user = pick(decoded, 'username', 'email', 'password');
+                const newToken = generateToken(user);
+                db.collection('users')
+                    .findOneAndUpdate({ token }, { $set: { token: newToken }})
+                    .then(() => {
+                        res.setHeader('authorization', newToken);
+                        next();
+                    })
+                    .catch(() => {
+                        next();
+                    });
+            });
         })
         .catch(err => {
             res.status(401).json({ message: err });
