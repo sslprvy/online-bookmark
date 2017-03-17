d36defb235860f4be33f6e104da4faf41054c696
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-17 21:36:39 +0100

    Define isLoggedIn substate

--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -1,14 +1,20 @@
 const defaultState = {
-    token: ''
+    token: '',
+    isLoggedIn: false
 };
 
 export default function(state = defaultState, action) {
     switch (action.type) {
         case 'UPDATE_TOKEN':
             return {
-                token: action.token
+                token: action.token,
+                isLoggedIn: state.isLoggedIn
             };
-
+        case 'LOGIN':
+            return {
+                isLoggedIn: true,
+                token: state.token
+            };
         default:
             return state;
     }
