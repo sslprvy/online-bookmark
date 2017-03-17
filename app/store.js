15cc5c6df535de61395ca491adb896acca5c3994
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-17 19:13:43 +0100

    OB-34: Put the store into its own file

--- app/store.js
+++ app/store.js
@@ -1,13 +1,15 @@
 import appReducers from './reducers/index';
+import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-let store = createStore(
+let appStore = createStore(
     appReducers,
     composeEnhancers(
         applyMiddleware(thunkMiddleware)
     )
 );
 
+export const dispatch = appStore.dispatch;
+export let store = appStore;
