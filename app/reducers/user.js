80766ddfbd530c664cfc970af08b5d8258678972
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-18 16:22:21 +0100

    OB-35: Init state for user handling

---
+++ app/reducers/user.js
@@ -0,0 +1,14 @@
+const defaultState = {
+    userCreated: false
+};
+
+export default function userAccount(state = defaultState, action) {
+    switch(action.type) {
+        case 'USER_CREATED':
+            return {
+                userCreated: true
+            };
+        default:
+            return state;
+    }
+}
