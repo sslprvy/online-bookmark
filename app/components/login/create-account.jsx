4790c2bfab4acdc54797761ee4972f3da78d2659
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-18 11:11:22 +0100

    OB-35: Convert CreateAccount componont to a stateful compoponent

--- app/components/login/create-account.jsx
+++ app/components/login/create-account.jsx
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const CreateAccount = ({ onLinkClick }) => {
+export default class CreateAccount extends Component {
+    constructor() {
+        super();
 
-    return (
-        <form className="register-form hide-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <input type="text" placeholder="email address"/>
-            <button>create</button>
-            <p className="message">Already registered? <a href="#" onClick={onLinkClick.bind(null, 'register-form')}>Sign In</a></p>
-        </form>
-    );
-};
+        this.state = {
+            createAccountFormState: {
+                username: '',
+                password: '',
+                email: ''
+            },
+            isValidPassword: false,
+            isValidUsername: false,
+            isValidEmail: false
+        };
 
-export default connect()(CreateAccount);
+        this.handleOnBlur = this.handleOnBlur.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    handleOnBlur(event) {
+        const validationFunctionToCall = `validate${capitalizeFirstLetter(event.target.name)}`;
+        let newState = Object.assign({}, this.state);
+        let subState = `isValid${capitalizeFirstLetter(event.target.name)}`;
+
+        if (this[validationFunctionToCall](event.target.value)) {
+            newState[subState] = true;
+        } else {
+            newState[subState] = false;
+        }
+
+        this.setState(newState);
+    }
+
+    handleOnChange(event) {
+        let newState = Object.assign({}, this.state);
+        newState.createAccountFormState[event.target.name] = event.target.value;
+
+        this.setState(newState);
+    }
+
+    validateUsername(value) {
+        return true;
+    }
+
+    validatePassword(value) {
+        return true;
+    }
+
+    validateEmail(value) {
+        // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+        var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegExp.test(value);
+    }
+
+    render() {
+        let opts = {};
+        if (!(this.state.isValidEmail || this.isValidPassword || this.isValidUsername)) {
+            opts['disabled'] = 'disabled';
+        }
+
+        return (
+            <form className="register-form hide-form" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="username" name="username"
+                       onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+                <input type="password" placeholder="password" name="password"
+                       onChange={this.handleOnChange} onBlur={this.handleOnBlur}/>
+                <input type="text" placeholder="email address" name="email" onBlur={this.handleOnBlur}/>
+                <button {...opts}>create</button>
+                <p className="message">Already registered? <a href="#" onClick={this.props.onLinkClick.bind(null, 'register-form')}>Sign In</a></p>
+            </form>
+        );
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
