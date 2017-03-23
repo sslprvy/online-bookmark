92168155167948ed835260fe89e40295b39d5d8d
Csaba Valyi <csaba.valyi1@gmail.com>
2017-03-23 20:44:57 +0100

    OB-39: Make EditableListElement stateful

--- app/components/editable-list-element.jsx
+++ app/components/editable-list-element.jsx
@@ -1,58 +1,56 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
+import { dispatch } from '../store';
 
 import { saveEntry } from '../actions/entry';
-import { updateUser } from '../actions/app-data';
+import { updateList } from '../actions/app-data';
 
-const mapDispatch = (dispatch) => {
-    return {
-        saveEntry: (id, listElement) => {
-            dispatch(saveEntry({id, listElement}));
-        },
-        updateUser: ({ user, id }) => {
-            dispatch(updateUser({ user, id }));
-        }
-    };
-};
+export default class EditableListElement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listElement: props.listElement
+        };
 
-const EditableListElement = ({ saveEntry, user, updateUser, listElement, listId }) => {
-    function updateEntry(event) {
-        const property = event.target.name;
-        const userData = user.elements.map(element => {
-            if (element === listElement) {
-                let newProperty = {};
-                newProperty[property] = event.target.value;
-                return Object.assign({}, element, Object.assign({}, element, newProperty));
-            } else {
-                return element;
-            }
-        });
-
-        const newUser = Object.assign({}, user, { data: userData });
-        updateUser({ user: newUser, id: listElement.id });
+        this.updateEntry = this.updateEntry.bind(this);
+        this.saveEntry = this.saveEntry.bind(this);
     }
 
-    return (
-        <li className="display-list-element">
-            <div className="display-list-element-content">
-                <input value={listElement.title} onChange={updateEntry} name="title" />
-                <input value={listElement.url} onChange={updateEntry} name="url" />
-            </div>
-            <div className="display-list-element-control">
-                <i className="fa fa-floppy-o display-list-element-remove-button"
-                    aria-hidden="true"
-                    onClick={saveEntry.bind(null, listId, listElement)}></i>
-                <i className="fa fa-times display-list-element-cancel-button"
-                    aria-hidden="true"></i>
-            </div>
-        </li>
-    );
-};
+    updateEntry(event) {
+        const property = event.target.name;
+        let updatedProperty = {};
+        updatedProperty[property] = event.target.value;
 
-EditableListElement.propTypes = {
-    user: PropTypes.object.isRequired,
-    listElement: PropTypes.object.isRequired,
-    listId: PropTypes.string.isRequired
-};
+        this.setState({
+            listElement: Object.assign({}, this.state.listElement, updatedProperty)
+        });
+    }
 
-export default connect(null, mapDispatch)(EditableListElement);
+    saveEntry() {
+        const updatedElements = this.props.list.elements.map(
+            element => element._id === this.state.listElement._id ? this.state.listElement : element
+        );
+        const updatedList = Object.assign({}, this.props.list, { elements: updatedElements });
+
+        dispatch(updateList({ list: updatedList, id: this.state.listElement._id }));
+        dispatch(saveEntry({ id: updatedList._id, listElement: this.state.listElement }));
+    }
+
+    render() {
+        return (
+            <li className="display-list-element">
+                <div className="display-list-element-content">
+                    <input value={this.state.listElement.title} onChange={this.updateEntry} name="title" />
+                    <input value={this.state.listElement.url} onChange={this.updateEntry} name="url" />
+                </div>
+                <div className="display-list-element-control">
+                    <i className="fa fa-floppy-o display-list-element-remove-button"
+                        aria-hidden="true"
+                        onClick={this.saveEntry}></i>
+                    <i className="fa fa-times display-list-element-cancel-button"
+                        aria-hidden="true"></i>
+                </div>
+            </li>
+        );
+    }
+}
