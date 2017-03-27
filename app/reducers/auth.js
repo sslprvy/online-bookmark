ba6be3475ea1ceca6dd2feb36952c23a551fbb69
Csaba Valyi <csaba_valyi@epam.com>
2017-03-27 07:40:01 +0200

    OB-45: Use action types from module in user.js reducer

--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -8,13 +8,13 @@ const defaultState = {
 
 export default function(state = defaultState, action) {
     switch (action.type) {
-        case 'UPDATE_TOKEN':
+        case UPDATE_TOKEN:
             return {
                 token: action.token,
                 isLoggedIn: state.isLoggedIn,
                 userName: state.userName
             };
-        case 'LOGGED_IN':
+        case LOGGED_IN:
             return {
                 isLoggedIn: true,
                 token: state.token,
--- app/reducers/entry.js
+++ app/reducers/entry.js
@@ -8,18 +8,18 @@ const defaultState = {
 
 export default function editEntry(state = defaultState, action) {
     switch (action.type) {
-        case 'NEW_ENTRY':
+        case NEW_ENTRY:
             return {
                 editing: true,
                 newEntry: true
             };
-        case 'EDIT_ENTRY':
+        case EDIT_ENTRY:
             return {
                 editing: true,
                 underEdit: action.id
             };
-        case 'ADD_ENTRY':
-        case 'SAVE_ENTRY':
+        case ADD_ENTRY:
+        case SAVE_ENTRY:
             return {
                 editing: false,
                 newEntry: false,
